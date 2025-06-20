9364b1c46a3641cbdd66812c769243cc267d3609
Joan Pere <orjoanp@gmail.com>
2025-06-20 19:34:10 +0200

    Canvi nom carpeta PRO1

--- Q1. PRO1/P0P1-Intro/X64734-Hola_i_adeu.cc
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Hola i Adeu!" << endl;
-}
--- Q1. PRO1/P5-Functions/simplRacional.cc
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-void read_rational(int& num, int& den)
-{
-}
